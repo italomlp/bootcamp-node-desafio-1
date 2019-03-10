788af841084637be51e6e2f205b177f7528d45d6
Italo Menezes <italomlporoca@hotmail.com>
2019-03-10 13:10:47 -0300

    add logic for major/minor

--- index.js
+++ index.js
@@ -8,8 +8,36 @@ nunjucks.configure('views', {
   watch: true
 })
 
+app.use(express.urlencoded({ extended: false }))
 app.set('view engine', 'njk')
 
+const isAgeInformedMiddleware = (req, res, next) => {
+  if (req.query.age) {
+    return next()
+  }
+  return res.redirect('/')
+}
+
 app.get('/', (req, res) => res.render('home', { name: 'Italo' }))
+
+app.post('/check', (req, res) => {
+  const { age } = req.body
+
+  if (age) {
+    if (age < 18) {
+      return res.redirect(`/minor?age=${age}`)
+    }
+    return res.redirect(`/major?age=${age}`)
+  }
+  return res.redirect('/')
+})
+
+app.get('/major', isAgeInformedMiddleware, (req, res) =>
+  res.render('major', { age: req.query.age })
+)
+
+app.get('/minor', isAgeInformedMiddleware, (req, res) =>
+  res.render('minor', { age: req.query.age })
+)
 
 app.listen(3000)
